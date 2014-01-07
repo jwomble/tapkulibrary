0c2bbd7e248f726dd993551527bd60b5eaf0b5bb
Jeff Womble <jwomble@wombatdesigns.com>
2014-01-07 16:24:54 -0600

    Fixed parse error by renaming method: - (void) motionEnded:withEvent:(UIEventSubtype)motion withEvent:(UIEvent *)event     to: - (void) motionEndedwithEventSubtype:(UIEventSubtype)motion withEvent:(UIEvent *)event

--- src/TapkuLibrary/TKWindow.m
+++ src/TapkuLibrary/TKWindow.m
@@ -47,7 +47,7 @@ NSString *TKWindowShakeEnded = @"motionEnded";
 - (void) motionCancelled:(UIEventSubtype)motion withEvent:(UIEvent *)event {
 	[[NSNotificationCenter defaultCenter] postNotificationName:TKWindowShakeCancelled object:self];
 }
-- (void) motionEnded:withEvent:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+- (void) motionEndedwithEventSubtype:(UIEventSubtype)motion withEvent:(UIEvent *)event {
 	[[NSNotificationCenter defaultCenter] postNotificationName:TKWindowShakeEnded object:self];
 }
 
